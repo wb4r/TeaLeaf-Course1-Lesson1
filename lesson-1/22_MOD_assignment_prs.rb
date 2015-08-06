01ef2b49030e10473ab855f8120939085466491d
Willem Toledano <willemtoledano@gmail.com>
2015-08-06 17:34:12 +0000

    Assignment 22 PRS Ready for review

--- lesson-1/22_MOD_assignment_prs.rb
+++ lesson-1/22_MOD_assignment_prs.rb
@@ -1,43 +1,39 @@
-# MODIFIED after the video solution
-replay = 'y'
+replay = true
+input_user = true
+input_comp = true
 CHOICES = {'p' => 'Paper', 'r' => 'Rock', 's' => 'Scissors'}
-
 
 # Creates a separation line to make it easier visually
 def separation()
   puts "------------------------------------------------"
 end
 
+# Method to play or not again. Attached at the end of LOOP
+def play_again?
+  replay = gets.chomp
+  replay == 'y'
+end
 
 puts "Let's play Paper, Rock, Scissors against the Computer!"
 separation()
 
-
-def prs_game()
-    
-  # Var declarations
-  input_user = true
-  input_comp = true
-    
+# MAIN GAME
+while replay
   puts "-> Choose an option: P/R/S"
-    
     
   # Save both picks. The user input and the computer Random (between 1 and 3)
   input_user = gets.downcase.chomp
   input_comp = CHOICES.keys.sample
     
-    
-  # While loop in case the user picks a wrong choice != PRS
+  # While loop if USER picks a WRONG choice != PRS
   while CHOICES.keys.include?(input_user) == false do 
     puts "-> Please choose again. Wrong option."
     input_user = gets.downcase.chomp
   end
-    
-    
-  # Print picks upcased to easy reading
+  
+  # PRINT picks upcased to easy reading
   puts "-> The user choose: #{input_user.upcase}"
   puts "-> And the computer's option is: #{input_comp.upcase}"
-    
     
   # Establishing the RESULT value based on both inputs        
   if input_user == input_comp then puts "---> Oh! Oh! TIE!!"
@@ -45,22 +41,16 @@ def prs_game()
       then puts "---> THE USER WINS!!"
   else puts "---> THE COMPUTER WINS!!" end
   
-  # This is extra. Could go out actually. I was practicing hash.each do |key, value|
+  # This is extra. Could go out actually. I was practicing "hash.each do |key, value|"
   CHOICES.each do |k, v|
     if input_user == k then input_user = CHOICES[k][v] end
     if input_comp == k then input_comp = CHOICES[k][v] end
   end
+  
   puts "---> It was #{input_user} vs. #{input_comp}"
-    
-end
-
-
-# We put the game (prs()) inside a loop to make it replayable
-while replay == 'y' || replay == 'Y' do
-  prs_game()
   puts "-> Do you want to play again? y/n"
-  replay = gets.chomp
-  if replay == 'n' then puts "-> Oh..OK. Good Bye boring fella!" end
-  separation()
+  
+  replay = play_again?
+  separation
 end
 
