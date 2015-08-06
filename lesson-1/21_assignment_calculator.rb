4d4ffc45cd69ca920a6b10caed513a80691968be
Willem Toledano <willemtoledano@gmail.com>
2015-08-06 16:10:43 +0000

    21 Assignment - Calculator MODified according to TA recomendations

--- lesson-1/21_assignment_calculator.rb
+++ lesson-1/21_assignment_calculator.rb
@@ -1,5 +1,8 @@
 # Variable declarations
-input_number1, input_number2, calculation, total = 0
+input_number1 = 0
+input_number2 = 0
+calculation = 0
+total = 0
 
 # Here comes the text and the numbers inputs, as well as the calculation option
 puts "This is a Calculator. You'll be asked to provide 2 numbers and what to do with them."
@@ -7,39 +10,28 @@ puts "Provide the first number: "
 input_number1 = gets.chomp
 puts "Provide the second number: "
 input_number2 = gets.chomp
-puts "Choose the number referencing the calculation you wish to do: "
-puts "1--> +    2--> -    3--> *    4-->/"
-calculation = gets.chomp
 
-# Converting to Float if a '.' is detected in the input value
-if input_number1.include?(".") || input_number2.include?(".")
-  input_number1 = input_number1.to_f
-  input_number2 = input_number2.to_f
-else
-  input_number1 = input_number1.to_i
-  input_number2 = input_number2.to_i
+# Options to calculate. Wont break if != (1..4)
+loop do 
+  puts "Choose the number referencing the calculation you wish to do: "
+  puts "1--> +    2--> -    3--> *    4-->/"
+  calculation = gets.chomp
+  break if (1..4).include?(calculation.to_i)
+  puts "Please, choose a correct option and try again."
 end
 
 # Here the result will be calculated based on the calculation option choosen
 case calculation
 when '1'
-  total = input_number1 + input_number2
+  total = input_number1.to_i + input_number2.to_i
 when '2'
-  total = input_number1 - input_number2
+  total = input_number1.to_i - input_number2.to_i
 when '3'
-  total = input_number1 * input_number2
+  total = input_number1.to_i * input_number2.to_i
 when '4'
   total = input_number1.to_f / input_number2.to_f
-else
-    puts "Please, choose a correct option and try again."
 end
 
 # Prints the result
 puts "The result is: #{total}"
 
-
-
-# DOUBTS:
-
-# => If input1 = 6.3, input2 = 6 and calculation = 2 the result is 0.2999999 instead
-# of 0.3
--- lesson-1/tests.rb
+++ lesson-1/tests.rb
@@ -2,34 +2,26 @@ def is_numeric_string(string)
   string =~ /\A\d+\z/ ? true : false
 end
 
-# Initialize variables (Scope).
 num1 = 0
 num2 = 0
 operation = 0
 
-# While inputs are different from string formed by digits keeps asking integers.
 loop do
   puts "Please input first integer:"
   num1 = gets.chomp
   puts "Please input second integer:"
   num2 = gets.chomp
-  if is_numeric_string(num1) && is_numeric_string(num2)
-    break
-  end
+  break if is_numeric_string(num1) && is_numeric_string(num2)
   puts "[!] Please input an integer"
 end
 
-# While input is different from 1, 2, 3 or 4 keeps asking for an integer.
 loop do
   puts "Select: 1 (Add) - 2 (Substract) - 3 (Multiply) - 4 (Divide)"
   operation = gets.chomp
-  if (1..4).include?(operation.to_i)
-    break
-  end
+  break if (1..4).include?(operation.to_i)
   puts "[!] Please input a valid number [1 2 3 4]"
 end
 
-# Calculate result according to operation.
 result =
   case operation
   when "1" then num1.to_i + num2.to_i
@@ -38,5 +30,4 @@ result =
   when "4" then num1.to_f / num2.to_f
   end
 
-# Print operation result.
 puts "Result: #{result}"
