e0b3c8c39123d3853b45f47a9634ae2eab4f681a
Willem Toledano <willemtoledano@gmail.com>
2015-08-06 08:13:04 +0000

    Adds PRS Game Assignment

---
+++ lesson-1/22_assignment_prs.rb
@@ -0,0 +1,72 @@
+replay = 'y'
+
+
+# Creates a separation line to make it easier visually
+def separation()
+    puts "------------------------------------------------"
+end
+
+
+puts "Let's play Paper, Rock, Scissors against the Computer!"
+separation()
+
+
+def prs()
+    
+    # Var declarations
+    # result meanings --> 1 = User wins  2 = Computer wins   3 = Tie
+    input_user, input_comp, result = true
+    
+    puts "-> Choose an option: P/R/S"
+    
+    
+    # Save both picks. The user input and the computer Random (between 1 and 3)
+    input_user = gets.downcase.chomp
+    input_comp = Random.new.rand(1..3)
+    
+    
+    # While loop in case the user picks a wrong choice != PRS
+    while input_user != 'p' && input_user != 'r' && input_user != 's' do 
+        puts "-> Please choose again. Wrong option."
+        input_user = gets.downcase.chomp
+    end
+    
+    
+    # Equaling computer inputs to make Ifs easier later
+    if input_comp == 1 then input_comp = 'p'
+        elsif input_comp == 2 then input_comp = 'r'
+    else input_comp = 's' end
+    
+    
+    # Print picks upcased to easy reading
+    puts "-> The user choose: #{input_user.upcase}"
+    puts "-> And the computer's option is: #{input_comp.upcase}"
+    
+    
+    # Establishing the RESULT value based on both inputs        
+    if input_user == input_comp then result = 3
+        elsif input_user == 'p' && input_comp == 'r' then result = 1
+        elsif input_user == 'p' && input_comp == 's' then result = 2
+        elsif input_user == 'r' && input_comp == 'p' then result = 2
+        elsif input_user == 'r' && input_comp == 's' then result = 1
+        elsif input_user == 's' && input_comp == 'p' then result = 1
+        elsif input_user == 's' && input_comp == 'r' then result = 2
+    end
+    
+    
+    # Message display based on RESULT
+    if result == 1 then puts "-> THE USER WINS!!"       
+        elsif result == 2 then puts "-> THE COMPUTER WINS!!"
+    else puts "-> Oh! Oh! TIE!!" end
+end
+
+
+# We put the game (prs()) inside a loop to make it replayable
+while replay == 'y' || replay == 'Y' do
+    prs()
+    puts "-> Do you want to play again? y/n"
+    replay = gets.chomp
+    if replay == 'n' then puts "-> Good Bye boring fella!" end
+        separation()
+end
+
